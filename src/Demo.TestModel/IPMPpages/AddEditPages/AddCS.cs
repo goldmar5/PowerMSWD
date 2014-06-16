9eaa0ef1b0101a3d567720368cb811b5300dc52e
https://goldmar5@github.com <goldmar5@users.noreply.github.com>
2014-06-16 17:21:54 +0300

    up

---
+++ src/Demo.TestModel/IPMPpages/AddEditPages/AddCS.cs
@@ -0,0 +1,90 @@
+﻿#region Usings - System
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion
+#region Usings - SWD
+using Swd.Core;
+using Swd.Core.Pages;
+using Swd.Core.WebDriver;
+#endregion
+#region Usings - WebDriver
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+#endregion
+namespace Demo.TestModel.IPMPpages
+{
+    public class AddCS : AddEditBasePage
+    {
+
+        public AddCS()
+        {
+            expectedCaption = "ADD CENTRAL STATION";
+        }
+
+        #region WebElements        
+
+        [FindsBy(How = How.CssSelector, Using = @"#centralstationAddBusyButtonTop")]
+        protected IWebElement btnSaveChangesAddCSTop { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = @"#centralstationAddBusyButtonBottom")]
+        protected IWebElement btnSaveChangesAddCSBottom { get; set; }
+
+        #endregion
+
+        #region Open()
+        public override void Open()
+        {
+            var loginPage = GetLoginPage();
+            var tycoPage = loginPage.Login();
+            var systemPage = tycoPage.System();
+            var CentralStationSPage = systemPage.CentralStations();
+            CentralStationSPage.AddCSClick();
+        }
+
+        #endregion
+
+        public override void VerifyExpectedElementsAreDisplayed()
+        {
+            #region General Header locators
+            VerifyElementVisible("tabPanels", tabPanels);
+            VerifyElementVisible("tabGroups", tabGroups);
+            VerifyElementVisible("tabEvents", tabEvents);
+            VerifyElementVisible("tabProcesses", tabProcesses);
+            VerifyElementVisible("tabSystem", tabSystem);
+            VerifyElementVisible("labelVersion", labelVersion);
+            VerifyElementVisible("labelCurrentUser", labelCurrentUser);
+            VerifyElementVisible("linkSettings", linkSettings);
+            VerifyElementVisible("linkLogout", linkLogout);
+            VerifyElementVisible("linkHelp", linkHelp);
+            #endregion
+
+            #region Caption locator
+            VerifyElementVisible("labelCaption", labelCaption);
+            #endregion
+
+            #region Add Edit Page locators
+            VerifyElementVisible("labelStayOnPage", labelStayOnPage);
+            VerifyElementVisible("checkboxStayOnPageTop", checkboxStayOnPageTop);
+            VerifyElementVisible("checkboxStayOnPageBottom", checkboxStayOnPageBottom);
+            VerifyElementVisible("btnSaveChangesTop", btnSaveChangesAddCSTop);
+            VerifyElementVisible("btnSaveChangesBottom", btnSaveChangesAddCSBottom);
+            VerifyElementVisible("btnDiscardChanges", btnDiscardChanges);
+            VerifyElementVisible("addEditGrid", addEditGrid);
+            VerifyElementVisible("pagePathNextToCaption", pagePathNextToCaption);
+            #endregion
+        }
+
+        public override void WaitLoadPage()
+        {
+            Wait.UntilVisible(btnSaveChangesAddCSTop, 20000);
+            Wait.UntilDisapear(mainModalDialog, 20000);
+            if (!this.ItIsYou())
+            {
+                throw new NoSuchElementException("Expected: " + expectedCaption + ", Current: " + CurrentCaption());
+            }
+        }
+    }
+}
+
---
+++ src/Demo.TestModel/IPMPpages/AddEditPages/EditCS.cs
@@ -0,0 +1,91 @@
+﻿#region Usings - System
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion
+#region Usings - SWD
+using Swd.Core;
+using Swd.Core.Pages;
+using Swd.Core.WebDriver;
+#endregion
+#region Usings - WebDriver
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+#endregion
+namespace Demo.TestModel.IPMPpages
+{
+    public class EditCS : AddEditBasePage
+    {
+
+        public EditCS()
+        {
+            expectedCaption = "EDIT CENTRAL STATION";
+        }
+
+        #region WebElements        
+
+        [FindsBy(How = How.CssSelector, Using = @"#centralstationEditBusyButtonTop")]
+        protected IWebElement btnSaveChangesEditCSTop { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = @"#centralstationEditBusyButtonBottom")]
+        protected IWebElement btnSaveChangesEditCSBottom { get; set; }
+
+        #endregion
+
+        #region Open()
+        public override void Open()
+        {
+            var loginPage = GetLoginPage();
+            var tycoPage = loginPage.Login();
+            var systemPage = tycoPage.System();
+            var CentralStationSPage = systemPage.CentralStations();
+            CentralStationSPage.EditCSClick();
+        }
+
+        #endregion
+
+        public override void VerifyExpectedElementsAreDisplayed()
+        {
+            #region General Header locators
+            VerifyElementVisible("tabPanels", tabPanels);
+            VerifyElementVisible("tabGroups", tabGroups);
+            VerifyElementVisible("tabEvents", tabEvents);
+            VerifyElementVisible("tabProcesses", tabProcesses);
+            VerifyElementVisible("tabSystem", tabSystem);
+            VerifyElementVisible("labelVersion", labelVersion);
+            VerifyElementVisible("labelCurrentUser", labelCurrentUser);
+            VerifyElementVisible("linkSettings", linkSettings);
+            VerifyElementVisible("linkLogout", linkLogout);
+            VerifyElementVisible("linkHelp", linkHelp);
+            #endregion
+
+            #region Caption locator
+            VerifyElementVisible("labelCaption", labelCaption);
+            #endregion
+
+            #region Add Edit Page locators
+            VerifyElementVisible("labelStayOnPage", labelStayOnPage);
+            VerifyElementVisible("checkboxStayOnPageTop", checkboxStayOnPageTop);
+            VerifyElementVisible("checkboxStayOnPageBottom", checkboxStayOnPageBottom);
+            VerifyElementVisible("btnSaveChangesTop", btnSaveChangesEditCSTop);
+            VerifyElementVisible("btnSaveChangesBottom", btnSaveChangesEditCSBottom);
+            VerifyElementVisible("btnDiscardChanges", btnDiscardChanges);
+            VerifyElementVisible("addEditGrid", addEditGrid);
+            VerifyElementVisible("pagePathNextToCaption", pagePathNextToCaption);
+            #endregion
+        }
+
+        public override void WaitLoadPage()
+        {
+            Wait.UntilVisible(btnSaveChangesEditCSTop, 20000);
+            Wait.UntilDisapear(mainModalDialog, 20000);
+            if (!this.ItIsYou())
+            {
+                throw new NoSuchElementException("Expected: " + expectedCaption + ", Current: " + CurrentCaption());
+            }
+        }
+    }
+}
+
+
